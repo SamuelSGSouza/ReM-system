3cad58f060cc5ee55580dd77de6a20451589dc5a
samuel santos <samuels.g.desouza@gmail.com>
2022-10-05 14:20:03 -0300

    some changes

--- extracoes/views.py
+++ extracoes/views.py
@@ -44,7 +44,7 @@ class Extracao(LoginRequiredMixin, ListView):
     def get_context_data(self,**kwargs):
         context = super().get_context_data(**kwargs)
         context['tags'] = form_models.Tags.objects.all()
-        context['teste'] = settings.MEDIA_ROOT #TODO: remover esta linha
+        context['teste'] = settings.MEDIA_URL#TODO: remover esta linha
         return context
 
     def post(self, *args, **kwargs):
